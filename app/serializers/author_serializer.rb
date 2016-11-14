1415cf2e3c16a58bc14d5c77cecbdf77eaed3e11
Eli Thorkelson <ethorkel@whittier.edu>
2016-11-14 14:14:06 -0800

    Serializers: Author serializer should include term and note associations

--- app/serializers/author_serializer.rb
+++ app/serializers/author_serializer.rb
@@ -4,7 +4,11 @@ class AuthorSerializer < ActiveModel::Serializer
     :description
 
   has_many :works
+  has_many :terms
+  has_many :notes
 
   link(:self) { author_path(object.id) }
   link(:works) { works_path(author_id: object.id) }
+  link(:terms) { terms_path(author_id: object.id) }
+  link(:notes) { notes_path(author_id: object.id) }
 end
